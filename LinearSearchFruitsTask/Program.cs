fa80f87b31b8b492efd7832053feedd6c3f9821a
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2025-02-05 18:49:27 +0000

    Update Program.cs

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -5,6 +5,45 @@
         static void Main(string[] args)
         {
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
+            Console.Writeline("what word would you like to enter");
+            string word = Console.Readline();
+            if (fruits[0] == word)
+            {
+                Console.Writeline("True")
+            }
+            
+            else if (fruits[1] == word)
+            {
+                Console.Writeline("true")
+            }
+            else 
+            {
+                Console.Writeline("false")
+            }
+            else if (fruits[2] == word)
+            {
+                Console.Writeline("true")
+            }
+            else if (fruits[3] == word)
+            {
+                Console.Writeline("true")
+            }
+            else if (fruits[4] == word)
+            {
+                Console.Writeline("true")
+            }
+            else if (fruits[5] == word)
+            {
+                Console.Writeline("true")
+            }
+            else 
+            {
+                  Console.Writeline("false")                        
+            }
+
+                                   
+                                
+                                  
 
             // Your program should:
             // Ask the user what word they would like to find
